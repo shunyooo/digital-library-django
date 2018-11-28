645b061f9ad604fd67738a69fc676efc80385eef
shunyooo <syunyo1028@yahoo.co.jp>
2018-11-28 21:55:58 +0900

    Update view

--- library/book/urls.py
+++ library/book/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name='book'
 urlpatterns = [
-    path('', views.BookListView.as_view(), name='index'),
+    path('', views.HomeView.as_view(), name='index'),
     path('<int:pk>/', views.BookDetailView.as_view(), name='detail'),
     path('upload', views.FileFieldView.as_view(), name='upload'),
 ]
--- library/book/views.py
+++ library/book/views.py
@@ -1,10 +1,12 @@
+from django.db.models import Count
 from django.http import HttpResponse, HttpResponseServerError
 from django.shortcuts import get_object_or_404, render
 from django.utils.decorators import method_decorator
+from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
 from book.forms import FileFieldForm
-from book.models import Book
+from book.models import Book, Tag
 from book.tasks import handle_uploaded_file
 from django.views.generic.edit import FormView
 
@@ -12,18 +14,33 @@ from django.views.generic import ListView, DetailView
 
 import logging
 
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        # 新着, 適当なタグを探して紐づく本, 10件を10回ぐらいループ
+        book_section_list = [
+            {"title": "新着アップロード", "list": Book.objects.order_by('-created_at').all()[:10]}
+        ]
+
+        # TODO: ここで集計せずbook_countでちゃんと入れておくように
+        for tag in Tag.objects.annotate(_book_count=Count('books')).filter(_book_count__gt=0).all()[:10]:
+            book_section_list.append({
+                "title": tag.content,
+                "list": tag.books.all()[:10],
+            })
+
+        context = {'book_section_list': book_section_list}
+        return render(request, "book/index.html", context=context)
+
 
 class BookListView(ListView):
     model = Book
-    template_name = "book/index.html"
+    
+    template_name = "book/list.html"
+
 
 class BookDetailView(DetailView):
     model = Book
     template_name = "book/detail.html"
-
-def book_detail(request, book_id):
-    book = get_object_or_404(Book, pk=book_id)
-    return render(request, 'book/detail.html', {'book': book})
 
 
 class FileFieldView(FormView):
