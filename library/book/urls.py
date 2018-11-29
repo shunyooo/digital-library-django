5635538c408b905e736de31d42993add185160c4
shunyooo <syunyo1028@yahoo.co.jp>
2018-11-29 23:09:56 +0900

    Add 検索結果画面

--- library/book/urls.py
+++ library/book/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name='book'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
+    path('list', views.BookListView.as_view(), name='list'),
     path('<int:pk>/', views.BookDetailView.as_view(), name='detail'),
     path('upload', views.FileFieldView.as_view(), name='upload'),
 ]
