ab2b9cdd3c7e229d7905d80f2bb0b4a4a0a5e639
shunyooo <syunyo1028@yahoo.co.jp>
2019-04-09 03:56:13 +0900

    add small image

--- library/book/models.py
+++ library/book/models.py
@@ -65,6 +65,7 @@ class Book(models.Model):
     pdf_file = models.FileField(max_length=500, null=True)
     zip_file = models.FileField(max_length=500, null=True)
     thumbnail_image = models.ImageField(max_length=500, null=True)
+    thumbnail_small_image = models.ImageField(max_length=500, null=True)
     page_count = models.PositiveIntegerField()
     author = models.ManyToManyField(Author, related_name='books', default=default_author, )
     tag = models.ManyToManyField(Tag, related_name='books', default=default_tag, )
