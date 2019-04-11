71f8b07d8eb29b751bd05e47f9f8c99f4e147750
shunyooo <syunyo1028@yahoo.co.jp>
2019-04-12 05:44:24 +0900

    fix static

--- library/library/settings.py
+++ library/library/settings.py
@@ -131,11 +131,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = (
+    [os.path.join(BASE_DIR, 'static')]
+)
 
 # For debugging
 if DEBUG:
