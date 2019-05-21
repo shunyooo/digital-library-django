64c602790132932271fe32989d3ea2735f4085fa
Syunyo Kawamoto <syunyo1028@yahoo.co.jp>
2019-05-21 20:59:13 +0900

    commentout collect static

--- etc/web/run.sh
+++ etc/web/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Collect static files
-echo "#### Collect static files"
-./manage.py collectstatic --noinput
+#echo "#### Collect static files"
+#./manage.py collectstatic --noinput
 
 # Apply database migrations
 echo "#### Apply database migrations"
